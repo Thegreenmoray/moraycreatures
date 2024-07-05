9c5f249699e9b16aec8df562440327662ecd627a
Thegreenmoray <112501898+Thegreenmoray@users.noreply.github.com>
2024-07-05 16:55:49 -0400

    Test passed

--- src/main/java/com/moray/moraymobs/tags/MorayKeys.java
+++ src/main/java/com/moray/moraymobs/tags/MorayKeys.java
@@ -15,7 +15,6 @@ public static final TagKey<EntityType<?>> ASSIMILABLE= registerEntityTag("assimi
 
     private static TagKey<EntityType<?>>  registerEntityTag(String name){
     return TagKey.create(Registries.ENTITY_TYPE,new ResourceLocation(MorayMobs.MODID,name));
-   vfdff
     }
 
 
