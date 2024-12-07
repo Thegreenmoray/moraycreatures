b4f7d80aa9c0a3c7f616d705647626e5cc5aa272
Thegreenmoray <112501898+Thegreenmoray@users.noreply.github.com>
2024-12-06 23:38:49 -0500

    Boost

--- src/main/java/com/moray/moraymobs/rendersandmodels/model/Soulcatchermodel.java
+++ src/main/java/com/moray/moraymobs/rendersandmodels/model/Soulcatchermodel.java
@@ -1,7 +1,6 @@
 package com.moray.moraymobs.rendersandmodels.model;
 
 import com.moray.moraymobs.MorayMobs;
-import com.moray.moraymobs.entity.living.animal.LavaPaddleFish;
 import com.moray.moraymobs.entity.living.monster.Soulcatcher;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
@@ -10,18 +9,17 @@ public class Soulcatchermodel extends GeoModel<Soulcatcher> {
 
     @Override
     public ResourceLocation getModelResource(Soulcatcher soulcatcher) {
-        return new ResourceLocation(MorayMobs.MODID,"geo/soulball.geo.json");
+        return new ResourceLocation(MorayMobs.MODID,"geo/soulcatcher.geo.json");
     }
 
     @Override
     public ResourceLocation getTextureResource(Soulcatcher soulcatcher) {
-        return new ResourceLocation(MorayMobs.MODID,"textures/entity/soulprojectile.png");
+        return new ResourceLocation(MorayMobs.MODID,"textures/entity/soulcatcher.png");
     }
 
     @Override
     public ResourceLocation getAnimationResource(Soulcatcher soulcatcher) {
-        return new ResourceLocation(MorayMobs.MODID,"animations/soulspin.json");
+        return new ResourceLocation(MorayMobs.MODID,"animations/soulcatcher.animation.json");
 
     }
-
 }
